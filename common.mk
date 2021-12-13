d2e6101d4ff904a5c51c0746ce0faf1f3c7d75d3
Kunmun <kunmun.devroms@gmail.com>
2021-12-13 14:22:52 +0530

    powerstats: Import AIDL HAL implementation
    • From https://android.googlesource.com/device/google/redbull/+/refs/tags/android-12.0.0_r16
    • Add sepolicies from https://android.googlesource.com/platform/hardware/google/pixel-sepolicy/+/refs/tags/android-12.0.0_r16
    • Add hidl fqname entry manifest for the same
    
    Change-Id: I76203dbb4e054e80e3833b62853e9ba5bbb07ff2
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>

--- common.mk
+++ common.mk
@@ -13,7 +13,8 @@ PRODUCT_PACKAGES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.pixel-libperfmgr
+    android.hardware.power-service.pixel-libperfmgr \
+    android.hardware.power.stats@1.0-service.pixel
 
 # Powerhint
 PRODUCT_COPY_FILES += \
